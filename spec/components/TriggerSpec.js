0ae52c2835b51cc8e1117743ed92fae95cffd102
rodrigo-morais <morais.rm@gmail.com>
2017-02-28 17:03:34 +0100

    Configuring ESlint to run after the tests.

--- spec/components/TriggerSpec.js
+++ spec/components/TriggerSpec.js
@@ -1,14 +1,14 @@
-import { expect } from 'chai';
-import React from 'react';
-import Trigger from 'src/components/Trigger';
-import { shallow } from 'enzyme';
+import { expect } from 'chai'
+import React from 'react'
+import Trigger from 'src/components/Trigger'
+import { shallow } from 'enzyme'
 
 describe('Trigger Component', () => {
-  const text = 'Select';
-  const props = { text };
-  const trigger = shallow(<Trigger {...props} />);
+  const text = 'Select'
+  const props = { text }
+  const trigger = shallow(<Trigger {...props} />)
 
   it('renders a text', () => {
-    expect(trigger.props().children).to.be.equal(text);
-  });
-});
+    expect(trigger.props().children).to.be.equal(text)
+  })
+})
--- src/components/Trigger.js
+++ src/components/Trigger.js
@@ -1,13 +1,13 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes } from 'react'
 
 function Trigger({ text }) {
   return (
     <a>{text}</a>
-  );
+  )
 }
 
 Trigger.propTypes = {
   text: PropTypes.string.isRequired
-};
+}
 
-export default Trigger;
+export default Trigger
